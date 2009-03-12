f3f3de50cd8d158f59f3e387e76e62795f2b307c
TS Lim <tslim@flexnode.com>
2009-03-13 00:56:17 +0800

    Optimization to cached objects

--- lib/timeline_fu/cache.rb
+++ lib/timeline_fu/cache.rb
@@ -3,15 +3,15 @@ require 'yaml'
 module TimelineFu::Cache
   
   def cached_actor
-    get_cached_object(:actor)
+    @_cached_actor ||= get_cached_object(:actor)
   end
   
   def cached_subject
-    get_cached_object(:subject)
+    @_cached_subject ||= get_cached_object(:subject)
   end
   
   def cached_secondary_subject
-    get_cached_object(:secondary_subject)
+    @_cached_secondary_subject ||=  get_cached_object(:secondary_subject)
   end
   
   def get_cached_object(obj)
